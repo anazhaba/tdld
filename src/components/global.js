b7cc02f5117699fceae3778ca90852863dfbcf92
anazhaba <Anazhaba@yandex.ru>
2024-07-19 16:41:31 +0300

    header final (maybe)

---
+++ src/components/global.js
@@ -0,0 +1,15 @@
+export const globals = {
+  home: {
+    title: "Toadland - Главная",
+    heading: "Главная",
+  },
+  about: {
+    title: "About Us",
+    heading: "Learn More About Us",
+  },
+  contact: {
+    title: "Contact Us",
+    heading: "Get in Touch",
+  },
+  // Add more pages as needed
+};
