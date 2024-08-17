8d5d965b3b0facf3b80ba8e527bb9c5ec152a704
anazhaba <Anazhaba@yandex.ru>
2024-08-17 11:39:38 +0300

    rss test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import react from "@astrojs/react"; // Импортируем рендерер для React
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tdld.vercel.app",
   integrations: [sitemap()],
+  integrations: [react()], // Добавляем React как интеграцию
 });
