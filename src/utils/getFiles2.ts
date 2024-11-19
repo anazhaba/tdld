a6b7257a3f9c7f23a490b3baa3c799c93d472401
anazhaba <Anazhaba@yandex.ru>
2024-11-19 21:11:01 +0300

    updadtemods

--- src/utils/getFiles2.ts
+++ src/utils/getFiles2.ts
@@ -1,13 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-
-export function getFiles(directory: string): { name: string; path: string }[] {
+export function getFiles2(directory: string): { name: string; path: string }[] {
   const files = fs.readdirSync(directory);
 
 
   return files.map((file) => ({
     name: file,
-    path: path.join('/mods', file)
+    path: path.join('/mods/lastUpdate', file)
     
   }));
 }
