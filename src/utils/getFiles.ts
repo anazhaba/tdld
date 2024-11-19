028bab2d4c4d0cbf66e5a3fca67457bc99f6ccca
anazhaba <Anazhaba@yandex.ru>
2024-11-19 21:23:44 +0300

    fix folder download

--- src/utils/getFiles.ts
+++ src/utils/getFiles.ts
@@ -2,12 +2,14 @@ import fs from 'fs';
 import path from 'path';
 
 export function getFiles(directory: string): { name: string; path: string }[] {
-  const files = fs.readdirSync(directory);
-
-
-  return files.map((file) => ({
-    name: file,
-    path: path.join('/mods', file)
+    const files = fs.readdirSync(directory)
+        .filter(file => {
+            const fullPath = path.join(directory, file);
+            return fs.statSync(fullPath).isFile();
+        });
     
-  }));
+    return files.map((file) => ({ 
+        name: file, 
+        path: path.join('/mods', file) 
+    }));
 }
