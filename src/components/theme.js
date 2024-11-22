6dcfcd37798f9e2d4a34fba5c996a63b00af86f8
kate <katepgoesintothesunset@gmail.ru>
2024-11-22 20:54:56 +0300

    fix: fix website theme

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,40 +1,37 @@
 const applyTheme = (theme) => {
-  document.documentElement.setAttribute("data-theme", theme);
-  localStorage.setItem("site-theme", theme);
-  document.documentElement.style.colorScheme = theme;
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('site-theme', theme);
+    document.documentElement.style.colorScheme = theme;
 };
 
 const initTheme = () => {
-  const prefersDarkMode = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  const savedTheme =
-    localStorage.getItem("site-theme") || (prefersDarkMode ? "dark" : "light");
-  applyTheme(savedTheme);
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const savedTheme = localStorage.getItem('site-theme') || (prefersDarkMode ? 'dark' : 'light');
+    applyTheme(savedTheme);
+    document.documentElement.style.visibility =
+        'visible'; /* Отображаем страницу обратно страницу до установки темы */
 };
 
 // Применяем тему перед подготовкой страницы
-document.addEventListener("astro:after-swap", initTheme);
+document.addEventListener('astro:after-swap', initTheme);
 // Также инициализируем при первоначальной загрузке
 initTheme();
 
 // Обработчик изменения системной темы
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", (e) => {
-    const newTheme = e.matches ? "dark" : "light";
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    const newTheme = e.matches ? 'dark' : 'light';
     applyTheme(newTheme);
-  });
+});
 const toggleTheme = () => {
-  const currentTheme = document.documentElement.getAttribute("data-theme");
-  const newTheme = currentTheme === "dark" ? "light" : "dark";
-  document.documentElement.setAttribute("data-theme", newTheme);
-  localStorage.setItem("site-theme", newTheme);
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('site-theme', newTheme);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  const themeToggleButton = document.getElementById("theme-toggle");
-  if (themeToggleButton) {
-    themeToggleButton.addEventListener("click", toggleTheme);
-  }
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', toggleTheme);
+    }
 });
