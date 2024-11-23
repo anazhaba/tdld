447494b6b595392f5ac8d689ff20fd149feeaf29
anazhaba <Anazhaba@yandex.ru>
2024-11-23 17:56:52 +0300

    ada

--- src/components/theme.js
+++ src/components/theme.js
@@ -11,6 +11,7 @@ const initTheme = () => {
   const savedTheme =
     localStorage.getItem("site-theme") || (prefersDarkMode ? "dark" : "light");
   applyTheme(savedTheme);
+<<<<<<< Updated upstream
   document.documentElement.style.visibility =
     "visible"; /* Отображаем страницу обратно страницу до установки темы */
 };
@@ -18,6 +19,13 @@ const initTheme = () => {
 // Применяем тему перед подготовкой страницы
 document.addEventListener("astro:after-swap", initTheme);
 // Также инициализируем при первоначальной загрузке
+=======
+  document.documentElement.style.visibility = "visible";
+};
+
+document.addEventListener("astro:after-swap", initTheme);
+
+>>>>>>> Stashed changes
 initTheme();
 
 // Обработчик изменения системной темы
