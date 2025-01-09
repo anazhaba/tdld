1a18e8c9e1e5bb4c59342466e02be8f9c8f909af
anazhaba <Anazhaba@yandex.ru>
2025-01-09 21:34:53 +0300

    test ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,15 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< Updated upstream
   site: "https://toadland.ru",
+=======
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }), // обязательно, без предварительно отрисованных страниц
+  site: "https://sh1z.online",
+>>>>>>> Stashed changes
   integrations: [sitemap()],
   integrations: [react()], // Добавляем React как интеграцию
   prefetchAll: true,
