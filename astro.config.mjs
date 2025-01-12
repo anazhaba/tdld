a1d5c4d07279036c525cdd4824efe16c8ad08092
anazhaba <Anazhaba@yandex.ru>
2025-01-12 20:28:56 +0300

    test ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static", 
+  output: "server",
+  adapter: node({
+    mode: "standalone", // или 'middleware', если нужно интегрировать с другими фреймворками
+  }),
   site: "https://toadland.ru",
   integrations: [sitemap()],
   prefetch: true,
